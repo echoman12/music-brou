b98ddf3a5cbf9016be2542e6297760689eee4882
echoman12 <45900323+echoman12@users.noreply.github.com>
2018-12-31 22:32:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = 'b';
+const prefix = '$';
 client.login(process.env.BOT_TOKEN); 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 client.on('ready', () => {
-  client.user.setGame('bhelp.','https://www.twitch.tv/peery13');
+  client.user.setGame('$help.','https://www.twitch.tv/peery13');
 });
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
